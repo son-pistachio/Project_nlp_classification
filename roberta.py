6a94345b412c097a67399bcc199ee41b3f50d3e9
son-pistachio <alias.mr.son@gmail.com>
2024-09-24 14:18:52 +0900

    update files

--- roberta.py
+++ roberta.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import os
 import random
 import numpy as np
@@ -5,13 +6,19 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import pandas as pd
+import wandb
+import datetime
+import yaml
 from pytorch_lightning import LightningModule, Trainer, seed_everything
+from pytorch_lightning.loggers import WandbLogger
 from torch.utils.data import DataLoader, Dataset
 from transformers import AutoModel, AutoTokenizer, AdamW, get_linear_schedule_with_warmup
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score, accuracy_score
+from pytorch_lightning.callbacks import ModelCheckpoint
 
-torch.set_float32_matmul_precision('high')
+if torch.__version__ >= '2.0':
+    torch.set_float32_matmul_precision('high')
 
 num_seed = 42
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -30,7 +37,20 @@ def set_seed(seed: int):
 
 set_seed(num_seed)
 
+# YAML 파일에서 설정 읽기
+def load_config(yaml_file):
+    with open(yaml_file, 'r') as file:
+        config = yaml.safe_load(file)
+        
+        lr = config['lr']
+        batch_size = config['batch_size']
+        max_epochs = config['max_epochs']
+        max_len = config['max_len']
+        num_classes = config['num_classes']
+    return float(lr), batch_size, max_epochs, max_len, num_classes
+
 df = pd.read_csv("/home/son/ml/hanyang/datasets/final_data.csv")
+lr, batch_size, max_epochs, max_len, num_classes = load_config('config.yaml')
 
 # train, val(15%), test(15%)
 train_df, temp_df = train_test_split(df, test_size=0.3, random_state=num_seed, stratify=df['label1'])
@@ -50,7 +70,7 @@ class TokenDataset(Dataset):
         text = self.data.iloc[idx]['text']
         label = self.data.iloc[idx]['label1']
         tokens = self.tokenizer(
-            text, return_tensors='pt', truncation=True, padding='max_length', add_special_tokens=True
+            text, return_tensors='pt', truncation=True, padding='max_length', max_length=max_len, add_special_tokens=True
         )
         input_ids = tokens['input_ids'].squeeze(0)
         attention_mask = tokens['attention_mask'].squeeze(0)
@@ -61,17 +81,19 @@ train_data = TokenDataset(train_df, CHECKPOINT_NAME)
 val_data = TokenDataset(val_df, CHECKPOINT_NAME)
 test_data = TokenDataset(test_df, CHECKPOINT_NAME)
 
-train_loader = DataLoader(train_data, batch_size=16, shuffle=False, num_workers=4)
-val_loader = DataLoader(val_data, batch_size=16, shuffle=False, num_workers=4)
-test_loader = DataLoader(test_data, batch_size=16, shuffle=False, num_workers=4)
+num_workers = multiprocessing.cpu_count() // 2
+train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
-class BertLightningModel(LightningModule):
-    def __init__(self, bert_pretrained, num_labels=12, lr=1e-5):
-        super(BertLightningModel, self).__init__()
+class roBertLightningModel(LightningModule):
+    def __init__(self, bert_pretrained, num_labels=num_classes, lr=lr, total_steps=None):
+        super(roBertLightningModel, self).__init__()
         self.save_hyperparameters()
         self.bert = AutoModel.from_pretrained(bert_pretrained)
         self.fc = nn.Linear(768, num_labels)
         self.loss_fn = nn.CrossEntropyLoss()
+        self.total_steps = total_steps
 
     def forward(self, input_ids, attention_mask):
         output = self.bert(input_ids=input_ids, attention_mask=attention_mask)
@@ -89,10 +111,12 @@ class BertLightningModel(LightningModule):
     def validation_step(self, batch, batch_idx):
         inputs, labels = batch
         outputs = self(**inputs)
+        loss = self.loss_fn(outputs, labels)
         _, preds = torch.max(outputs, dim=1)
         self.val_predictions.append(preds)
         self.val_targets.append(labels)
-        self.val_losses.append(self.loss_fn(outputs, labels))
+        self.val_losses.append(loss)
+        self.log('val_loss_step', loss, prog_bar=True)
 
     def on_validation_epoch_start(self):
         self.val_predictions = []
@@ -105,8 +129,9 @@ class BertLightningModel(LightningModule):
         loss = torch.mean(torch.stack(self.val_losses))
 
         # 분산 환경에서 모든 프로세스의 결과를 모음
-        preds = self.all_gather(preds)
-        targets = self.all_gather(targets)
+        if self.trainer.world_size > 1:
+            preds = self.all_gather(preds)
+            targets = self.all_gather(targets)
 
         preds = preds.cpu().numpy()
         targets = targets.cpu().numpy()
@@ -134,8 +159,9 @@ class BertLightningModel(LightningModule):
         targets = torch.cat(self.test_targets)
 
         # 분산 환경에서 모든 프로세스의 결과를 모음
-        preds = self.all_gather(preds)
-        targets = self.all_gather(targets)
+        if self.trainer.world_size > 1:
+            preds = self.all_gather(preds)
+            targets = self.all_gather(targets)
 
         preds = preds.cpu().numpy()
         targets = targets.cpu().numpy()
@@ -153,9 +179,31 @@ class BertLightningModel(LightningModule):
             num_warmup_steps=0,
             num_training_steps=self.trainer.estimated_stepping_batches
         )
-        return [optimizer], [scheduler]
+        return [optimizer], [{'scheduler': scheduler, 'interval': 'step'}]
 
-bert_model = BertLightningModel(CHECKPOINT_NAME)
-trainer = Trainer(max_epochs=10, deterministic=True)
+# WandbLogger 설정
+now_sys = datetime.datetime.now().strftime("%m%d_%H%M")
+wandb_logger = WandbLogger(project="bert-classification", log_model=True, name="roberta_"+now_sys)
+
+checkpoint_callback = ModelCheckpoint(
+    monitor='val_loss',
+    dirpath='checkpoints/bert/',
+    filename='bert-{epoch:02d}-{val_loss:.2f}',
+    save_top_k=1,
+    mode='min',
+)
+
+trainer = Trainer(
+    max_epochs=max_epochs,
+    deterministic=True,
+    logger=wandb_logger,
+    callbacks=[checkpoint_callback]
+
+)
+total_steps = len(train_loader) * trainer.max_epochs
+bert_model = roBertLightningModel(CHECKPOINT_NAME, total_steps=total_steps)
+
 trainer.fit(bert_model, train_loader, val_loader)
 trainer.test(bert_model, test_loader)
+
+wandb.finish()
--- sbert.py
+++ sbert.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import os
 import random
 import numpy as np
@@ -6,6 +7,8 @@ import torch.nn as nn
 import torch.optim as optim
 import pandas as pd
 import wandb
+import datetime
+import yaml
 from pytorch_lightning import LightningModule, Trainer, seed_everything
 from pytorch_lightning.loggers import WandbLogger
 from torch.utils.data import DataLoader, Dataset
@@ -13,8 +16,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score, accuracy_score
 from transformers import AdamW, get_linear_schedule_with_warmup
 from sentence_transformers import SentenceTransformer  # For SBERT
+from pytorch_lightning.callbacks import ModelCheckpoint
 
-torch.set_float32_matmul_precision('high')
+if torch.__version__ >= '2.0':
+    torch.set_float32_matmul_precision('high')
 
 num_seed = 42
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -33,7 +38,20 @@ def set_seed(seed: int):
 
 set_seed(num_seed)
 
+# YAML 파일에서 설정 읽기
+def load_config(yaml_file):
+    with open(yaml_file, 'r') as file:
+        config = yaml.safe_load(file)
+        
+        lr = config['lr']
+        batch_size = config['batch_size']
+        max_epochs = config['max_epochs']
+        max_len = config['max_len']
+        num_classes = config['num_classes']
+    return float(lr), batch_size, max_epochs, max_len, num_classes
+
 df = pd.read_csv("/home/son/ml/hanyang/datasets/final_data.csv")
+lr, batch_size, max_epochs, max_len, num_classes = load_config('config.yaml')
 
 # train, val(15%), test(15%)
 train_df, temp_df = train_test_split(df, test_size=0.3, random_state=num_seed, stratify=df['label1'])
@@ -61,19 +79,21 @@ train_data = SBERTDataset(train_df, CHECKPOINT_NAME)
 val_data = SBERTDataset(val_df, CHECKPOINT_NAME)
 test_data = SBERTDataset(test_df, CHECKPOINT_NAME)
 
-train_loader = DataLoader(train_data, batch_size=16, shuffle=False, num_workers=4)
-val_loader = DataLoader(val_data, batch_size=16, shuffle=False, num_workers=4)
-test_loader = DataLoader(test_data, batch_size=16, shuffle=False, num_workers=4)
+num_workers = multiprocessing.cpu_count() // 2
+train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
 # Lightning 모듈 정의
 class SBERTLightningModel(LightningModule):
-    def __init__(self, sbert_model_name, num_labels=12, lr=1e-5):
+    def __init__(self, sbert_model_name, num_labels=num_classes, lr=lr, total_steps=None):
         super(SBERTLightningModel, self).__init__()
         self.save_hyperparameters()
         # SBERT returns embeddings, so we don't need a full transformer model here
         self.sbert_model = SentenceTransformer(sbert_model_name)
         self.fc = nn.Linear(768, num_labels)  # SBERT's embedding size is 768
         self.loss_fn = nn.CrossEntropyLoss()
+        self.total_steps = total_steps
 
     def forward(self, embeddings):
         logits = self.fc(embeddings)
@@ -91,10 +111,12 @@ class SBERTLightningModel(LightningModule):
     def validation_step(self, batch, batch_idx):
         inputs, labels = batch
         outputs = self(**inputs)
+        loss = self.loss_fn(outputs, labels)
         _, preds = torch.max(outputs, dim=1)
         self.val_predictions.append(preds)
         self.val_targets.append(labels)
-        self.val_losses.append(self.loss_fn(outputs, labels))
+        self.val_losses.append(loss)
+        self.log('val_loss_step', loss, prog_bar=True)
 
     def on_validation_epoch_start(self):
         self.val_predictions = []
@@ -107,8 +129,9 @@ class SBERTLightningModel(LightningModule):
         loss = torch.mean(torch.stack(self.val_losses))
 
         # 분산 환경에서 모든 프로세스의 결과를 모음
-        preds = self.all_gather(preds)
-        targets = self.all_gather(targets)
+        if self.trainer.world_size > 1:
+            preds = self.all_gather(preds)
+            targets = self.all_gather(targets)
 
         preds = preds.cpu().numpy()
         targets = targets.cpu().numpy()
@@ -136,8 +159,9 @@ class SBERTLightningModel(LightningModule):
         targets = torch.cat(self.test_targets)
 
         # 분산 환경에서 모든 프로세스의 결과를 모음
-        preds = self.all_gather(preds)
-        targets = self.all_gather(targets)
+        if self.trainer.world_size > 1:
+            preds = self.all_gather(preds)
+            targets = self.all_gather(targets)
 
         preds = preds.cpu().numpy()
         targets = targets.cpu().numpy()
@@ -155,20 +179,32 @@ class SBERTLightningModel(LightningModule):
             num_warmup_steps=0,
             num_training_steps=self.trainer.estimated_stepping_batches
         )
-        return [optimizer], [scheduler]
+        return [optimizer], [{'scheduler': scheduler, 'interval': 'step'}]
 
 # WandbLogger 설정
-wandb.init(project="sbert-classification")
-wandb_logger = WandbLogger(project="sbert-classification", log_model=True)
+now_sys = datetime.datetime.now().strftime("%m%d_%H%M")
+wandb_logger = WandbLogger(project="bert-classification", log_model=True, name="sbert"+now_sys)
 
-sbert_model = SBERTLightningModel(CHECKPOINT_NAME)
-trainer = Trainer(
-    max_epochs=10,
-    deterministic=True,
-    logger=wandb_logger
+checkpoint_callback = ModelCheckpoint(
+    monitor='val_loss',
+    dirpath='checkpoints/bert/',
+    filename='bert-{epoch:02d}-{val_loss:.2f}',
+    save_top_k=1,
+    mode='min',
 )
 
-trainer.fit(sbert_model, train_loader, val_loader)
-trainer.test(sbert_model, test_loader)
+trainer = Trainer(
+    max_epochs=max_epochs,
+    deterministic=True,
+    logger=wandb_logger,
+    callbacks=[checkpoint_callback]
+
+)
+
+total_steps = len(train_loader) * trainer.max_epochs
+bert_model = SBERTLightningModel(CHECKPOINT_NAME, total_steps=total_steps)
+
+trainer.fit(bert_model, train_loader, val_loader)
+trainer.test(bert_model, test_loader)
 
 wandb.finish()
